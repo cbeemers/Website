43095436561749245fec06923d25bbf76c088ea9
cbeemers <cbeems13@gmail.com>
2020-01-05 23:00:01 -0500

    minor styling changes

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Login(models.Model):
+    email = models.CharField(max_length=120, blank=True)
+    password = models.CharField(max_length=120, blank=True)
+
 class Signup(models.Model):
     email = models.CharField(max_length=120, blank=True)
     username = models.CharField(max_length=120, blank=False)
